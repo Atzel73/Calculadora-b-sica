c97d9ed19a36458e5a0e133fc34c4cd64c2b0fc4
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2021-10-31 18:22:24 -0600

    Add files via upload

---
+++ JOptionPane.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.calculadoratrabajo;
+
+/**
+ *
+ * @author Propietario
+ */
+class JOptionPane {
+
+    static void showMessageDialog(String el_cero_es_invalido_Por_favor_introduce_o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ main.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.calculadoratrabajo;
+
+/**
+ *
+ * @author Propietario
+ */
+public class main {
+    public static String Calculadora (String dato1, String dato2, String signo){
+        
+        Double resultado = 0.0;
+        String respuesta;
+        
+        if(signo.equals("+")){
+            resultado = Double.parseDouble(dato1) + Double.parseDouble(dato2);
+        }if(signo.equals("-")){
+            resultado = Double.parseDouble(dato1) - Double.parseDouble(dato2);
+        }if(signo.equals("*")){
+            resultado = Double.parseDouble(dato1) * Double.parseDouble(dato2);
+        }if(signo.equals("/")){
+            resultado = Double.parseDouble(dato1) / Double.parseDouble(dato2);
+        }
+        respuesta = resultado.toString();
+
+        return respuesta;
+    }
+    
+    public static boolean ExistePunto(String cadena){
+        boolean resultado;
+        resultado = false;
+        
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.substring(i, i+1).equals(".")) {
+                resultado = true;
+                break;
+            }
+        }
+        return resultado;
+    }
+    
+    
+}
